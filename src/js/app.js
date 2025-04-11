e86efcc97e69ae66ab65c68d2dffea0027a96ade
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-04-11 16:42:18 +0000

    Correcion Ternarios

--- src/js/app.js
+++ src/js/app.js
@@ -9,21 +9,31 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name ? variables.name : "Nombre"} ${variables.lastName ? variables.lastName : "Apellido"}</h1>
+          <h1>${variables.name ? variables.name : "Nombre"} ${
+    variables.lastName ? variables.lastName : "Apellido"
+  }</h1>
           <h2>${variables.role ? variables.role : "Rol profesional"}</h2>
           <h3>${variables.city ? variables.city : "Ciudad"}</h3>
           <h3>${variables.country ? variables.country : "País"}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://x.com/${variables.twitter ? variables.twitter : ''}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github ? variables.github : ''}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/in/${variables.linkedin ? variables.linkedin : ''}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : ''}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://x.com/${
+              variables.twitter ? variables.twitter : ""
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : ""
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${
+              variables.linkedin ? variables.linkedin : ""
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : ""
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
-window.onload = function () {
+window.onload = function() {
   window.variables = {
     includeCover: true,
     background:
@@ -39,13 +49,13 @@ window.onload = function () {
     lastName: null,
     role: null,
     country: null,
-    city: null,
+    city: null
   };
 
   render(window.variables);
 
-  document.querySelectorAll(".picker").forEach(function (elm) {
-    elm.addEventListener("change", function (e) {
+  document.querySelectorAll(".picker").forEach(function(elm) {
+    elm.addEventListener("change", function(e) {
       const attribute = e.target.getAttribute("for");
       let values = {};
       values[attribute] =
