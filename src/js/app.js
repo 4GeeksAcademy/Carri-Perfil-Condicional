f0cd4f9346bf18e55ae3472cf987f597a2dacd7e
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-04-11 16:34:34 +0000

    Correcion Ternarios

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,36 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("Variables actuales: ", variables);
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}</h3>
-          <h3>${variables.country}</h3>
+          <h1>${variables.name ? variables.name : "Nombre"} ${variables.lastName ? variables.lastName : "Apellido"}</h1>
+          <h2>${variables.role ? variables.role : "Rol profesional"}</h2>
+          <h3>${variables.city ? variables.city : "Ciudad"}</h3>
+          <h3>${variables.country ? variables.country : "País"}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://x.com/RealBetis"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://www.youtube.com/@RealBetis"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/company/real-betis-balompie/?originalSubdomain=es"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://www.instagram.com/realbetisbalompie/?hl=es"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://x.com/${variables.twitter ? variables.twitter : ''}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ? variables.github : ''}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin ? variables.linkedin : ''}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ? variables.instagram : ''}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
-window.onload = function() {
+window.onload = function () {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background:
       "https://diadelaprovincia.dipusevilla.es/export/sites/diadelaprovincia/.galleries/IMAGENES-Galardonados/IMG-20190524-WA0004.jpg",
-    // this is the url for the profile avatar
     avatarURL:
       "https://www.cdeportiva.com/wp-content/uploads/2022/04/Joaquin-Betis-1200x774.jpg",
-    // social media bar position (left or right)
     socialMediaPosition: "position-left",
-    // social media usernames
     twitter: null,
     github: null,
     linkedin: null,
@@ -71,14 +39,14 @@ window.onload = function() {
     lastName: null,
     role: null,
     country: null,
-    city: null
+    city: null,
   };
-  render(window.variables); // render the card for the first time
 
-  document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+  render(window.variables);
+
+  document.querySelectorAll(".picker").forEach(function (elm) {
+    elm.addEventListener("change", function (e) {
+      const attribute = e.target.getAttribute("for");
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -88,7 +56,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      render(Object.assign(window.variables, values));
     });
   });
 };
